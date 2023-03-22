6e2342a8e209284265b20e399b6451e8c188d48a
Basinger <fel_simon@mail.ru>
2023-03-22 23:36:15 +0300

    Added task about friends and dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int distance = 10000,
+firstFriendSpeed = 1,
+secondFriendSpeed = 2,
+dogSpeed = 5,
+friend = 2,
+count = 0,
+time = 0;
+
+while(distance > 5)
+{   
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed); // Высчитываем время, за которое собака пробежит расстояние от второго друга к первому
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed); // Вычисляем время, за которое собака пробежит расстояние от первого друга ко второму
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time; // Вычисляем расстояние, пройденное обоими друзьями за время, пока собака пробежала от 2 к 1, и от 1 ко 2 обратно
+
+    count += 1; // Счетчик количества перебежек собаки от одного друга ко второму
+}
+
+Console.WriteLine("Собака перебежит от одного друга к другому " + count + " раз");
