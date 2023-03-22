56b19455c7e97a12bb8aed33d8cbb5a9c3219b8f
Basinger <fel_simon@mail.ru>
2023-03-22 16:24:12 +0300

    added Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
