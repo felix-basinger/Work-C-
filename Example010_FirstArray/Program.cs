52552d3fda3be42c65f77086f319d3e85d3ecee4
Basinger <fel_simon@mail.ru>
2023-03-25 01:08:29 +0300

    Added Example010_FirstArray

---
+++ Example010_FirstArray/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] nums = {5, 1, 6, 8, 4};
+
+int a = nums.Min();
+Console.WriteLine("min = " + Array.IndexOf(nums, a));
+
+int b = nums.Max();
+Console.WriteLine("max = " + Array.IndexOf(nums, b));
