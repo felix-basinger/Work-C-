663791a158ff58199f40869eb3958853315784c4
Basinger <fel_simon@mail.ru>
2023-03-26 01:55:00 +0300

    Added Example012_IntroArray

---
+++ Example012_IntroArray/Program.cs
@@ -0,0 +1,84 @@
+﻿// #1
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if(arg2 > result) result = arg2;
+//     if(arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int [] array = {11,211,13,43,53,61,75,38,19};
+
+// int result = Max(
+//     Max(array[0], array[1], array[2]),
+//     Max(array[3], array[4], array[5]),
+//     Max(array[6], array[7], array[8])
+// );
+// Console.WriteLine(result);
+
+// #2
+
+// int[] array = { 13, 276, 23, 42, 18, 65, 47, 18, 93 };
+
+// int n = array.Length;
+// int find = 18;
+
+// int index = 0;
+
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
