61f7edaf4357b06c7096d30a7c9587af5d2ce961
Basinger <fel_simon@mail.ru>
2023-03-27 00:13:43 +0300

    Added Example013_TrainingCode

---
+++ Example013_TrainingCode/Program.cs
@@ -0,0 +1,54 @@
+﻿// void SayHelloRu()
+// {
+//     Console.WriteLine("Привет");
+// }
+// void SayHelloEn()
+// {
+//     Console.WriteLine("Hello");
+// }
+// void SayHelloFr()
+// {
+//     Console.WriteLine("Bonjour");
+// }
+
+// string language = "fr";
+
+// switch(language)
+// {
+//     case "en":
+//         SayHelloEn();
+//         break;
+//     case "ru":
+//         SayHelloRu();
+//         break;
+//     case "fr":
+//         SayHelloFr();
+//         break;
+// }
+
+int [] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
